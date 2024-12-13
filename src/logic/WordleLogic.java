cb0e545310bf6a5a1bed3d16a1c022f14efbe702
Favour F Atilade <favyatil@gmail.com>
2024-12-13 21:55:00 +0100

    WORDLE CLONE - clone of popular game wordle without an actual UI,played in   command line. - fix match Index method to deal with letters that occur more than   once in a word.

---
+++ src/logic/WordleLogic.java
@@ -0,0 +1,106 @@
+package logic;
+import domain.*;
+
+import java.util.Random;
+
+public class WordleLogic {
+    public StringBuilder builder;
+    public StringBuilder stringBuilder;
+    private final String wordOfTheDay;
+    private final Random rand;
+
+    public WordleLogic() {
+        stringBuilder = new StringBuilder("\n");
+        rand = new Random();
+        wordOfTheDay = getWordOfTheDay();
+        builder = new StringBuilder("LETTERS NOT IN WORD: #");
+    }
+
+    public String getWordOfTheDay() {
+        MyDictionary myDictionary = new MyDictionary();
+
+        //returns a random int as (index) to get word from the arraylist
+        int random = rand.nextInt(myDictionary.getSize());
+        return myDictionary.getIndex(random);
+    }
+
+    /*logic to check words and indexes: Players have six attempts to guess a five-letter word,
+     with feedback given for each guess in the form of colored tiles indicating when letters match or
+      occupy the correct position.
+      After every guess, each letter is marked as either green, yellow or gray:
+      green indicates that the letter is correct and in the correct position,
+      yellow means it is in the answer but not in the right position,
+      while gray indicates it is not in the answer at all.
+     */
+    public boolean checkWord(String word) {
+        String otherWord = word.toUpperCase();
+        return otherWord.equals(wordOfTheDay);
+    }
+
+
+    public String matchIndex(String input) {
+        //checks for letters that appear in words but at the wrong index
+        // arraylist is used to use List interface' indexOf method
+        char[] inputArray = input.toCharArray();
+        char[] charsOfTheDay = wordOfTheDay.toCharArray();
+
+        for (int m = 0; m < input.length(); m++) {
+            int index = wordOfTheDay.indexOf(input.charAt(m));
+            if (index == -1) {
+                sanitizeBuilder(input.charAt(m));
+            }else {
+                // check for second occurence of character
+                int index2 = wordOfTheDay.lastIndexOf(input.charAt(m));
+                if (index2 != index) { // if second occurence exist, check if we indexes are equal
+                    if (m == index) stringBuilder.append(input.charAt(m)).append(" ").append(rightIndexInWord());
+                    else if (m == index2) stringBuilder.append(input.charAt(m)).append(" ").append(rightIndexInWord());
+                    else stringBuilder.append(input.charAt(m)).append(" ").append(wrongIndexInWord());
+                } else {
+                    if (m == index) stringBuilder.append(input.charAt(m)).append(" ").append(rightIndexInWord());
+                    else stringBuilder.append(input.charAt(m)).append(" ").append(wrongIndexInWord());
+                }
+            }
+
+        }
+        return stringBuilder.toString();
+    }
+
+    private String rightIndexInWord() {
+        return  " is in the correct position. \n";
+    }
+
+    private String wrongIndexInWord() {
+        return " is in word but not in the correct position. \n";
+    }
+
+
+    public String getWord() {
+        return  wordOfTheDay;
+    }
+
+    // gurantees chars not in the word are not repeated
+    public void sanitizeBuilder(char s) {
+        String newString = builder.toString();
+        String[] sanitizedString = newString.split(": ");
+
+        if (sanitizedString.length > 1) {
+            char[] characters = sanitizedString[1].toCharArray();
+            boolean exists = false;
+
+            //for loop to iterate through all characters
+            for (int i = 0; i < characters.length; i++) {
+                if (characters[i] == s) {
+                    exists = true;
+                    break;
+                }
+            }
+            if (!exists) builder.append(s).append(" ");
+        }
+
+    }
+
+    //It is called by the method in user interface message after every full iteration,
+    public void clearString() {
+        stringBuilder.delete(0,stringBuilder.length()-1);
+    }
+}
---
+++ src/ui/UserInterface.java
@@ -0,0 +1,90 @@
+package ui;
+//import  Wordle.domain.*;
+import logic.*;
+import java.util.Scanner;
+
+public class UserInterface {
+    protected  WordleLogic logic;
+    private final Scanner scanner;
+    private static final int MAX = 6; // how many iterations to check
+
+
+    public UserInterface () {
+        this.intro();
+        this.scanner = new Scanner(System.in);
+
+        do {
+            this.logic = new WordleLogic();
+            this.start();
+
+            System.out.println("Would you like to play again?"
+                    +"Enter YES/NO.");
+        } while (scanner.nextLine().startsWith("Y"));
+        System.out.println("Thanks for playing. Auf Widersehen");
+        scanner.close();
+    }
+
+
+    // prints intro
+    private void intro() {
+        System.out.println("=================================");
+        System.out.println("WELCOME TO F-WORDLE\n " +
+                "Guess today's five letter <<word of The Day>> in 6 tries and you win");
+        System.out.println("=================================");
+        System.out.println("""
+                How to play:\s
+                PLAY WITH CAPS LOCK ON:\n
+                \t green indicates that the letter is correct and in the correct position.
+                      yellow means it is in the answer but not in the right position.
+                      while gray indicates it is not in the answer at all.""");
+    }
+
+    // start the game
+    private void start() {
+        System.out.println("START");
+        int count = 0;
+
+        while (count < MAX) {
+            System.out.println("Enter 5 letter word.");
+            String read = scanner.nextLine();
+
+            /* Ensures input is a 5-letter word and is not entered in lower case: */
+            while(read.length() != 5 || read.equals(read.toLowerCase())) {
+                System.out.println("Invalid input: Only 5 letter words in ALL CAPS"
+                + "\nEnter word again.");
+                read = scanner.nextLine();
+            }
+
+            boolean checkword = this.logic.checkWord(read);
+
+            if (checkword && count == 0) {
+                System.out.println("Unreal guess. You got it in " + count + 1 + " tries.");
+                //playAgain();
+                break;
+            } else if (checkword) {
+                System.out.println("Correct guess, You got today's word in " + (count + 1) + " tries." );
+                //playAgain();
+                break;
+            }else {
+                this.getMessage(read);
+                System.out.println(this.logic.builder +"\n " );
+                //resets String builder to an empty string
+                this.logic.clearString();
+            }
+
+            count++;
+        }
+
+        if (count == MAX) {
+            System.out.println("Sorry :( Word Of The Day is : " + this.logic.getWord());
+        }
+    }
+
+    public void getMessage(String input) {
+        String string;
+        string = this.logic.matchIndex(input);
+        System.out.println(string);
+    }
+
+
+}
